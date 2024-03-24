b42f0c3569cd4983fddd7835229ef23c69078c3a
Santosh <161675482+SantoshAdurthi@users.noreply.github.com>
2024-03-25 00:13:35 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -1,1 +1,16 @@
-
+def gitdownload (repo)
+{
+  https://github.com/SantoshAdurthi/${repo}.git
+}
+def mavenbuild ()
+{
+  sh 'mvn package'
+}
+def tomcatdeploy (jobname,private_ip_address,context)
+{
+  sh 'scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ${username}@${private_ip_address}:/var/lib/tomcat9/${context}.war'
+}
+def testing (jobname)
+{
+  sh 'java -jar /var/lib/jenkins/workspace/${jobname}/testing.jar'
+}
