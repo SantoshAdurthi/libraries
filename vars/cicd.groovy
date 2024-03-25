2f0a5d6fe33cdab779477a44438dcf2ef888a35a
Santosh <161675482+SantoshAdurthi@users.noreply.github.com>
2024-03-26 01:01:02 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -14,3 +14,28 @@ def testing (jobname)
 {
   sh 'java -jar /var/lib/jenkins/workspace/${jobname}/testing.jar'
 }
+def e1 ()
+{
+  mail bcc: '', body: 'Jenkins is unable to download the code from github', cc: '', from: '', replyTo: '', subject: 'Continuous Download failed', to: 'santosh.adurthi@gmail.com'
+  exit (1)
+}
+def e2 ()
+{
+   mail bcc: '', body: 'Jenkins is unable to build the .war file', cc: '', from: '', replyTo: '', subject: 'Continuous Build failed', to: 'sandy.kalwa@gmail.com'
+   exit (1)
+}
+def e3 ()
+{
+  mail bcc: '', body: 'Jenkins is unable to deploy the .war file', cc: '', from: '', replyTo: '', subject: 'Continuous Deployment failed', to: 'sandy.kalwa@gmail.com'
+  exit (1)
+}
+def e4 ()
+{
+  mail bcc: '', body: 'Jenkins is unable to run the selenium code', cc: '', from: '', replyTo: '', subject: 'Continuous Testing failed', to: 'adurthisravan@gmail.com'
+  exit (1)
+}
+def e5 ()
+{
+  mail bcc: '', body: 'Jenkins is unable to deploy the prodapp.war', cc: '', from: '', replyTo: '', subject: 'Continuous Delivery failed', to: 'adurthisravan@gmail.com'
+  exit (1)
+}
